2ebb2af68c1fd47d926310c012dec723f4fd7b0f
Lucas Machado Rodrigues <lucas050685@gmail.com>
2024-03-09 11:13:27 -0300

    feat: adding tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
+import path from 'path';
 import react from '@vitejs/plugin-react'
+
+const resolvedPath =  path.resolve(__dirname, './src') + '/';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '~/': resolvedPath,
+    }
+  },
   plugins: [react()],
   server: {
     port: 3002
