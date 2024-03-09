240ab0b577294a0fe51d6cf117df490913111b6a
Lucas Machado Rodrigues <lucas050685@gmail.com>
2024-03-09 11:48:22 -0300

    feat: add shad cn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Button } from "./front/components/ui/button";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Button variant="default">This is a default button</Button>
+      <Button variant="secondary">This is a secondary button</Button>
+      <Button variant="destructive">This is a destructive button</Button>
+      <Button variant="ghost">This is a ghost button</Button>
+      <Button variant="link">This is a link button</Button>
+      <Button variant="outline">This is a outline button</Button>
+      <Button size="icon">X</Button>
+    </>
   )
 }
