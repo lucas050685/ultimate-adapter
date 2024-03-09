cb0d88ecd721e1e000a79102d9213a15cb12af8a
Lucas Machado Rodrigues <lucas050685@gmail.com>
2024-03-09 10:57:26 -0300

    feat: add concurrently dev command

--- src/template.ts
+++
@@ -1,9 +0,0 @@
-export const template = `{
-  "address": "{{{address.street}}}, {{{address.city}}}, {{{address.state}}}, {{{address.country}}}, {{{address.zipCode}}}",
-  "age": {{age}},
-  "company": "{{{employment.company}}}",
-  "email": "{{{email}}}",
-  "interests": [{{#interests}}"{{{.}}}",{{/interests}}],
-  "jobTitle": "{{{employment.position}}}",
-  "name": "{{{firstName}}} {{{lastName}}}"
-}`;
